f2898abc04175233bc6892a1b1ae4de72c2e038a
sudeep-star9 <adhikarisudeep12@gmail.com>
2021-07-17 06:15:53 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+//using in object
+var Company={
+    Ktm:"Rc",
+    Bajaj:"ns",
+    Yamaha:"R6",
+    Honda:'Cbr600'
+}
+for(var i in Company){
+    console.log(Company[i])
+}
+//using in array
+var Company=[
+"Rc",
+"ns",
+"R6",
+'Cbr600'
+]
+for(var i in Company){
+    console.log(Company[i])
+}
